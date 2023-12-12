6dd9beb865e50cd92e82b7e25dd6e8240127d48d
Rodrigo Llanes <romadritex@gmail.com>
2023-12-12 06:40:19 +0100

    2023-12

--- 2023/12/So2.py
+++ 2023/12/So2.py
@@ -6,42 +6,44 @@ def parse(line: str):
     return list('?'.join([a] * 5)), list(map(int, b.split(','))) * 5
 
 
-def cache(f):
-    mem = {}
+mem = {}
 
-    def w(line, sizes, index, si, fun):
+
+def cache(f):
+    def w(line, sizes, index, si):
         if (index, si) not in mem:
-            mem[index, si] = f(line, sizes, index, si, fun)
+            mem[index, si] = f(line, sizes, index, si)
         return mem[index, si]
 
     return w
 
 
-def solve(line, sizes, index, si, rec):
+@cache
+def solve(line, sizes, index, si):
     if index >= len(line):
         return 1 if si == len(sizes) else 0
     elif line[index] == '.':
-        return rec(line, sizes, index + 1, si, rec)
+        return solve(line, sizes, index + 1, si)
     elif line[index] == '#':
         if si < len(sizes) and index + sizes[si] - 1 < len(line) and \
                 all(s == '#' or s == '?' for s in line[index: index + sizes[si]]) and \
                 (index + sizes[si] >= len(line) or line[index + sizes[si]] == '.' or line[index + sizes[si]] == '?'):
-            return rec(line, sizes, index + sizes[si] + 1, si + 1, rec)
+            return solve(line, sizes, index + sizes[si] + 1, si + 1)
         return 0
     elif line[index] == '?':
         c = 0
         if si < len(sizes) and index + sizes[si] - 1 < len(line) and \
                 all(s == '#' or s == '?' for s in line[index: index + sizes[si]]) and \
                 (index + sizes[si] >= len(line) or line[index + sizes[si]] == '.' or line[index + sizes[si]] == '?'):
-            c = rec(line, sizes, index + sizes[si] + 1, si + 1, rec)
-        return c + rec(line, sizes, index + 1, si, rec)
+            c = solve(line, sizes, index + sizes[si] + 1, si + 1)
+        return c + solve(line, sizes, index + 1, si)
 
 
 def main(lines: list) -> None:
     c = 0
     for a, b in lines:
-        sol = cache(solve)
-        c += sol(a, b, 0, 0, sol)
+        mem.clear()
+        c += solve(a, b, 0, 0)
     print(c)
 
 
